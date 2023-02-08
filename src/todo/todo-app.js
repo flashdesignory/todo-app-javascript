ace322562ed8d7a5dea990419c0cfbe192667d9e
Thorsten Kober <tkober@gmail.com>
2023-02-08 13:48:39 -0800

    more refactoring

--- src/todo/todo-app.js
+++ src/todo/todo-app.js
@@ -1,5 +1,5 @@
-import { TodoItem } from "./todo-item.js";
 import { TodoControls } from "./todo-controls.js";
+import { TodoList } from "./todo-list.js";
 import { TodoFilters } from "./todo-filters.js";
 import { useApi } from "./use-api.js";
 import { useRouter } from "./use-router.js";
@@ -7,9 +7,6 @@ import { useRouter } from "./use-router.js";
 export const TodoApp = ({ ref, data = [] }) => {
   const { getTodos, addItem, updateItem, removeItem, toggleItem, reset } = useApi(data);
   const { initRouter, getRoute } = useRouter();
-
-  // refs
-  const list = ref.querySelector(".todo-list-ul");
 
   // handlers
   const handleToggle = (id) => {
@@ -19,68 +16,38 @@ export const TodoApp = ({ ref, data = [] }) => {
 
   const handleUpdate = (id, task) => {
     updateItem(id, task);
-    list.focus();
     updateView();
   };
 
   const handleDelete = (id) => {
     removeItem(id);
-    document.getElementById(id).remove();
     updateView();
   };
 
   const handleAdd = (task) => {
-    const todo = addItem(task);
-    list.prepend(createItem(todo));
+    addToList(addItem(task));
     updateView();
   };
 
   const handleReset = () => {
     reset();
-    list.replaceChildren();
+    resetList();
     updateView();
   };
 
   const { update: updateControls } = TodoControls({ ref, getTodos, getRoute, onSubmit: handleAdd });
   const { update: updateFilters } = TodoFilters({ ref, getTodos, getRoute, onReset: handleReset });
-
-  // helpers
-  const createItem = (todo) =>
-    TodoItem({
-      todo,
-      onToggle: handleToggle,
-      onUpdate: handleUpdate,
-      onDelete: handleDelete,
-    });
+  const {
+    update: updateList,
+    reset: resetList,
+    add: addToList,
+  } = TodoList({ ref, getTodos, getRoute, onToggle: handleToggle, onUpdate: handleUpdate, onDelete: handleDelete });
 
   const updateView = () => {
-    const todos = getTodos();
-    const route = getRoute();
-
-    if (updateControls) updateControls();
-    if (updateFilters) updateFilters();
-
-    // update todos to show (depending on filter)
-    todos.forEach((todo) => {
-      const element = document.getElementById(todo.id);
-      let shouldShow = true;
-      switch (route) {
-        case "active":
-          shouldShow = !todo.completed;
-          break;
-        case "completed":
-          shouldShow = todo.completed;
-          break;
-        default:
-          shouldShow = true;
-      }
-      element.style.display = shouldShow ? "flex" : "none";
-    });
+    updateControls();
+    updateFilters();
+    updateList();
   };
-
-  // render initial todos
-  const todos = getTodos();
-  todos.map((todo) => list.append(createItem(todo)));
 
   // initialize router
   initRouter(updateView);
---
+++ src/todo/todo-list.js
@@ -0,0 +1,65 @@
+import { TodoItem } from "./todo-item.js";
+
+export const TodoList = ({ ref, getTodos, getRoute, onToggle, onUpdate, onDelete }) => {
+  // refs
+  const list = ref.querySelector(".todo-list-ul");
+
+  // handlers
+  const handleToggle = (id) => {
+    onToggle(id);
+  };
+
+  const handleUpdate = (id, task) => {
+    list.focus();
+    onUpdate(id, task);
+  };
+
+  const handleDelete = (id) => {
+    document.getElementById(id).remove();
+    onDelete(id);
+  };
+
+  const createItem = (todo) =>
+    TodoItem({
+      todo,
+      onToggle: handleToggle,
+      onUpdate: handleUpdate,
+      onDelete: handleDelete,
+    });
+
+  const update = () => {
+    const todos = getTodos();
+    const route = getRoute();
+
+    // update todos to show (depending on filter)
+    todos.forEach((todo) => {
+      const element = document.getElementById(todo.id);
+      let shouldShow = true;
+      switch (route) {
+        case "active":
+          shouldShow = !todo.completed;
+          break;
+        case "completed":
+          shouldShow = todo.completed;
+          break;
+        default:
+          shouldShow = true;
+      }
+      element.style.display = shouldShow ? "flex" : "none";
+    });
+  };
+
+  const reset = () => {
+    list.replaceChildren();
+  };
+
+  const add = (todo) => {
+    list.prepend(createItem(todo));
+  };
+
+  // render initial todos
+  const todos = getTodos();
+  todos.map((todo) => list.append(createItem(todo)));
+
+  return { update, reset, add };
+};
