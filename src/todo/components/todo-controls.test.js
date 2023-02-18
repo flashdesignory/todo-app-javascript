686ae56bb90fb3c009ed72066e6f5b82cfe44e1e
Thorsten Kober <tkober@gmail.com>
2023-02-18 13:19:45 -0800

    test optimization

--- src/todo/components/todo-controls.test.js
+++ src/todo/components/todo-controls.test.js
@@ -1,31 +1,14 @@
 import { TodoControls } from "./todo-controls";
+import { completedTodos } from "../test/data.js";
+import { controlsFragment } from "../test/fragments.js";
 
 describe("TodoControls", function () {
-  document.body.innerHTML = `
-        <div class="todo-controls">
-            <div class="todo-toggle-container hidden">
-                <input id="todo-toggle-element" type="checkbox" tabindex="0" />
-                <label class="visually-hidden" for="todo-toggle-element">Mark all as complete.</label>
-            </div>
-            <form class="todo-form">
-                <div class="todo-input-container">
-                    <label class="visually-hidden" for="todo-input-element">Todo Input</label>
-                    <input id="todo-input-element" class="todo-input" type="text" placeholder="What needs to be done?" autofocus autocomplete="off" />
-                </div>
-            </form>
-        </div>
-    `;
+  document.body.innerHTML = controlsFragment;
 
   it("should render", function () {
     const ref = document.querySelector(".todo-controls");
     const getRoute = () => "";
-    const getTodos = () => [
-      {
-        id: "6a3e2475-dd95-4125-9ca3-614e451169eb",
-        task: "Wash Car",
-        completed: true,
-      },
-    ];
+    const getTodos = () => [...completedTodos];
     const onSubmit = (value) => {console.log("value", value)};
 
     const { update } = TodoControls({ ref, getTodos, getRoute, onSubmit });
@@ -45,6 +28,5 @@ describe("TodoControls", function () {
     const input = document.querySelector("#todo-input-element");
     input.value = "foo";
     form.submit(); */
-    
   });
 });
