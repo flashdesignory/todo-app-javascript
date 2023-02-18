9612ad1cefdfd543c7d5cd9ae8d790eea242cde6
Thorsten Kober <tkober@gmail.com>
2023-02-18 09:28:44 -0800

    run format

--- src/todo/components/todo-controls.test.js
+++ src/todo/components/todo-controls.test.js
@@ -1,7 +1,7 @@
 import { TodoControls } from "./todo-controls";
 
-describe("TodoControls", function() {
-    document.body.innerHTML =`
+describe("TodoControls", function () {
+  document.body.innerHTML = `
         <div class="todo-controls">
             <div class="todo-toggle-container hidden">
                 <input id="todo-toggle-element" type="checkbox" tabindex="0" />
@@ -14,31 +14,31 @@ describe("TodoControls", function() {
                 </div>
             </form>
         </div>
-    `
+    `;
 
-    it("should render", function() {
-        const ref = document.querySelector(".todo-controls");
-        const getRoute = () => "";
-        const getTodos = () => [
-            {
-                id: "6a3e2475-dd95-4125-9ca3-614e451169eb",
-                task: "Wash Car",
-                completed: true,
-              }
-        ];
-        const onSubmit = () => {};
+  it("should render", function () {
+    const ref = document.querySelector(".todo-controls");
+    const getRoute = () => "";
+    const getTodos = () => [
+      {
+        id: "6a3e2475-dd95-4125-9ca3-614e451169eb",
+        task: "Wash Car",
+        completed: true,
+      },
+    ];
+    const onSubmit = () => {};
 
-        const { update } = TodoControls({ ref, getTodos, getRoute, onSubmit });
+    const { update } = TodoControls({ ref, getTodos, getRoute, onSubmit });
 
-        const toggleContainer = document.querySelector(".todo-toggle-container");
-        const toggle = document.querySelector("#todo-toggle-element");
+    const toggleContainer = document.querySelector(".todo-toggle-container");
+    const toggle = document.querySelector("#todo-toggle-element");
 
-        expect(toggleContainer.classList.contains("hidden")).toBeTruthy();
-        expect(toggle.checked).toBeFalsy();
+    expect(toggleContainer.classList.contains("hidden")).toBeTruthy();
+    expect(toggle.checked).toBeFalsy();
 
-        update();
-        
-        expect(toggleContainer.classList.contains("hidden")).toBeFalsy();
-        expect(toggle.checked).toBeTruthy();
-    });
+    update();
+
+    expect(toggleContainer.classList.contains("hidden")).toBeFalsy();
+    expect(toggle.checked).toBeTruthy();
+  });
 });
