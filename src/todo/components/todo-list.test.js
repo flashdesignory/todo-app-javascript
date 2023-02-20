e9bfa874104c650a877b9678ee99eb25059e15ea
Thorsten Kober <tkober@gmail.com>
2023-02-20 13:57:34 -0800

    update tests

--- src/todo/components/todo-list.test.js
+++ src/todo/components/todo-list.test.js
@@ -1,5 +1,5 @@
 import { TodoList } from "./todo-list.js";
-import { emptyTodos, mixedTodos } from "../test/data.js";
+import { emptyTodos, mixedTodos, newTodo } from "../test/data.js";
 import { createBodyFragment } from "../test/fragments.js";
 
 describe("TodoList", () => {
@@ -42,5 +42,101 @@ describe("TodoList", () => {
 
     const elements = ref.querySelectorAll(".todo-list-li");
     expect(elements.length).toEqual(3);
+  });
+
+  it("should only show active todos on update", () => {
+    document.body.innerHTML = createBodyFragment([...emptyTodos]);
+    const ref = document.querySelector(".todo-main");
+
+    getRoute.mockReturnValue("active");
+    getTodos.mockReturnValue([...mixedTodos]);
+
+    const { update } = TodoList({ ref, getTodos, getRoute, onToggle, onUpdate, onDelete });
+
+    update();
+
+    const elements = ref.querySelectorAll('.todo-list-li[style*="display: flex"]');
+    expect(elements.length).toEqual(2);
+  });
+
+  it("should only show completed todos on update", () => {
+    document.body.innerHTML = createBodyFragment([...emptyTodos]);
+    const ref = document.querySelector(".todo-main");
+
+    getRoute.mockReturnValue("completed");
+    getTodos.mockReturnValue([...mixedTodos]);
+
+    const { update } = TodoList({ ref, getTodos, getRoute, onToggle, onUpdate, onDelete });
+
+    update();
+
+    const elements = ref.querySelectorAll('.todo-list-li[style*="display: flex"]');
+    expect(elements.length).toEqual(1);
+  });
+
+  it("should remove completed todos from the list", () => {
+    document.body.innerHTML = createBodyFragment([...emptyTodos]);
+    const ref = document.querySelector(".todo-main");
+
+    getRoute.mockReturnValue("");
+    getTodos.mockReturnValue([...mixedTodos]);
+
+    const { update, remove } = TodoList({ ref, getTodos, getRoute, onToggle, onUpdate, onDelete });
+
+    update();
+
+    // before removal
+    let elements = ref.querySelectorAll(".todo-list-li");
+    expect(elements.length).toEqual(3);
+
+    remove('[data-completed="true"]');
+
+    // after removal
+    elements = ref.querySelectorAll(".todo-list-li");
+    expect(elements.length).toEqual(2);
+  });
+
+  it("should add a todo to the list", () => {
+    document.body.innerHTML = createBodyFragment([...emptyTodos]);
+    const ref = document.querySelector(".todo-main");
+
+    getRoute.mockReturnValue("");
+    getTodos.mockReturnValue([...mixedTodos]);
+
+    const { update, add } = TodoList({ ref, getTodos, getRoute, onToggle, onUpdate, onDelete });
+
+    update();
+
+    // before removal
+    let elements = ref.querySelectorAll(".todo-list-li");
+    expect(elements.length).toEqual(3);
+
+    add({ ...newTodo });
+
+    // after removal
+    elements = ref.querySelectorAll(".todo-list-li");
+    expect(elements.length).toEqual(4);
+  });
+
+  it("should empty list on reset", () => {
+    document.body.innerHTML = createBodyFragment([...emptyTodos]);
+    const ref = document.querySelector(".todo-main");
+
+    getRoute.mockReturnValue("");
+    getTodos.mockReturnValue([...mixedTodos]);
+
+    const { update, reset } = TodoList({ ref, getTodos, getRoute, onToggle, onUpdate, onDelete });
+
+    update();
+
+    // before removal
+    let elements = ref.querySelectorAll(".todo-list-li");
+    expect(elements.length).toEqual(3);
+
+    reset();
+
+    // after removal
+    elements = ref.querySelectorAll(".todo-list-li");
+    expect(elements.length).toEqual(0);
   });
 });
