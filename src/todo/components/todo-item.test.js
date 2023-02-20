838dd29e0469c11441c9bbfdd6cb2cf065f50ed0
Thorsten Kober <tkober@gmail.com>
2023-02-20 09:22:43 -0800

    todo item test

--- src/todo/components/todo-item.test.js
+++ src/todo/components/todo-item.test.js
@@ -8,10 +8,44 @@ describe("TodoItem", () => {
 
   beforeEach(() => {
     jest.resetAllMocks();
+    // fake timers for double click
+    jest.useFakeTimers();
   });
 
-  it("should return an item", () => {
-    const item = TodoItem({ todo: oneTodo[0], onToggle, onUpdate, onDelete });
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it("should return an item", async () => {
+    const todo = oneTodo[0];
+    const item = TodoItem({ todo, onToggle, onUpdate, onDelete });
+    document.body.append(item);
     expect(item).toBeTruthy();
+
+    // toggle
+    const toggleInput = item.querySelector(`#toggle-${todo.id}`);
+    expect(toggleInput).toBeTruthy();
+    toggleInput.click();
+    expect(onToggle).toHaveBeenCalledTimes(1);
+
+    // input
+    const taskInput = item.querySelector(`#task-${todo.id}`);
+    expect(taskInput).toBeTruthy();
+    expect(taskInput.readOnly).toBeTruthy();
+    taskInput.click();
+    jest.advanceTimersByTime(50);
+    taskInput.click();
+    jest.advanceTimersByTime(50);
+    expect(taskInput.readOnly).toBeFalsy();
+    taskInput.textContent = "Clean Car";
+    taskInput.blur();
+    expect(onUpdate).toHaveBeenCalledWith(todo.id, "Clean Car");
+    expect(taskInput.readOnly).toBeTruthy();
+
+    // delete
+    const deleteButton = item.querySelector(".todo-item-button");
+    expect(deleteButton).toBeTruthy();
+    deleteButton.click();
+    expect(onDelete).toHaveBeenCalledTimes(1);
   });
 });
